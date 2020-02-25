ed9aa663d2ae89090890dc269b823d72a115fa64
Narcology <51177216+Narcology@users.noreply.github.com>
2020-02-25 17:16:47 +0300

    Add files via upload

---
+++ ImaginariumInventory.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Inventory : MonoBehaviour
+{
+    private static Inventory instance;
+
+    public static Inventory Instance
+    {
+        get 
+        {
+            if (!instance)
+                instance = FindObjectOfType<Inventory>();
+            return instance; 
+        }
+    }
+
+
+    [SerializeField] private GameObject lamp;
+    [SerializeField] private GameObject tree;
+    [SerializeField] private GameObject rainbow;
+    [SerializeField] private GameObject apple;
+    [SerializeField] private GameObject gramophone;
+    [SerializeField] private GameObject bird;
+    [SerializeField] private GameObject saw;
+
+
+    private void Awake()
+    {
+        ClearInventory();
+    }
+
+    public void TakeLamp(bool take)
+    {
+        lamp.SetActive(take);
+    }
+    public void TakeTree(bool take)
+    {
+        tree.SetActive(take);
+    }
+
+
+    public void TakeRainbow(bool take)
+    {
+        rainbow.SetActive(take);
+    }
+
+    public void TakeApple(bool take)
+    {
+        apple.SetActive(take);
+    }
+
+    public void TakeGramophone(bool take)
+    {
+        gramophone.SetActive(take);
+    }
+
+    public void TakeBird(bool take)
+    {
+        bird.SetActive(take);
+    }
+
+    public void TakeSaw(bool take)
+    {
+        saw.SetActive(take);
+    }
+
+
+    public void ClearInventory()
+    {
+        lamp.SetActive(false);
+        tree.SetActive(false);
+        rainbow.SetActive(false);
+        apple.SetActive(false);
+        gramophone.SetActive(false);
+        bird.SetActive(false);
+        saw.SetActive(false);
+    }
+}
---
+++ ImaginariumPlayerController.cs
@@ -0,0 +1,54 @@
+﻿//code for PlayerController.cs
+
+private void OnObjectTaken(GameObject go)
+{
+    if (go.name == "Lamp")
+    {
+        ObjectManager.Instance.DestroyLampItem();
+        GameManager.IsLihgtsItemTaken = true;
+        inventory.TakeLamp(true);
+    }
+
+    if (go.name == "tree_item")
+    {
+        ObjectManager.Instance.DestroyTreeItem();
+        GameManager.IsObjectsItemTaken = true;
+        inventory.TakeTree(true);
+    }
+
+    if (go.name == "Rainbow")
+    {
+        ObjectManager.Instance.DestroyColorItem();
+        GameManager.IsColorItemTaken = true;
+        inventory.TakeRainbow(true);
+    }
+
+    if (go.name == "Apple")
+    {
+        ObjectManager.Instance.DestroyColorItem();
+        GameManager.IsPhysicsItemTaken = true;
+        inventory.TakeApple(true);
+    }
+
+    if (go.name == "Gramophone")
+    {
+        ObjectManager.Instance.DestroyGramophones();
+        GameManager.IsSoundsItemTaken = true;
+        inventory.TakeGramophone(true);
+    }
+
+    if (go.name == "bird")
+    {
+        ObjectManager.Instance.DestroyBirdItem();
+        GameManager.IsAnimalsItemTaken = true;
+        inventory.TakeBird(true);
+    }
+
+    if (go.name == "Saw")
+    {
+        ObjectManager.Instance.DestroySawItem();
+        GameManager.IsHumanityItemTaken = true;
+        inventory.TakeSaw(true);
+    }
+
+}
